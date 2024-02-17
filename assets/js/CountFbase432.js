818cd1c9b1ce3fdf216ccec4b36c60622b71b22e
Nguyễn Minh Đức <95899532+nminhducit@users.noreply.github.com>
2024-02-17 21:56:28 +0700

    Update and rename CountFbase432 to CountFbase432.js

--- assets/js/CountFbase432.js
+++ assets/js/CountFbase432.js
@@ -1,4 +1,4 @@
-<script>/*<![CDATA[*/
+/*<![CDATA[*/
 const wcViewCountFbase = {
   firebaseUrl: 'https://choipan-wendy-default-rtdb.firebaseio.com/',
   abbreviation: '1', // 0 or 1 or 2
@@ -7,4 +7,4 @@ const wcViewCountFbase = {
 };
   
 function viewCountLoad(){var script=document.createElement('script');script.async= true;script.src='https://cdn.wendycode.com/blogger/widget/viewcount.js';document.body.appendChild(script)}function vcDtcLzy(){viewCountLoad(),localStorage.setItem("viewCountJs","true")}var wcLdStorage=localStorage.getItem("viewCountJs");if("true"!=wcLdStorage){var t=!1,e=!1;window.addEventListener("scroll",()=>{(0!=document.documentElement.scrollTop&&!1===t||0!=document.body.scrollTop&&!1===t)&&(vcDtcLzy(),t=!0,e=!0)},!0),window.addEventListener("click",()=>{!1===e&&!1===e&&(vcDtcLzy(),e=!0,t=!0)},!0)}"true"===wcLdStorage&&viewCountLoad();"undefined"!= typeof infinite_scroll&&infinite_scroll.on("load",function(){viewCountLoad();});
-/*]]>*/</script>
+/*]]>*/
